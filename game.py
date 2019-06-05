d3dae7e6df1f2790627abe86aad2baeda4318e35
daecker <50798284+daecker@users.noreply.github.com>
2019-06-05 18:56:22 -0400

    Capture Inputs
    Write code to capture inputs

--- game.py
+++ game.py
@@ -2,3 +2,19 @@
 
 print("Rock, Paper, Scissors, Shoot!")
 
+# CAPTURE INPUTS
+
+user_choice = input("Please select Rock, Paper, or Scissors: ")
+print("---------")
+print("You chose", user_choice)
+
+# VALIDATE INPUTS
+
+
+# GENERATE COMPUTER SELECTION
+
+
+# DETERMINE THE WINNER
+
+
+# DISPLAY FINAL OUTPUTS / OUTCOMES
