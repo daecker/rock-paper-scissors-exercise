3c4ed61909a8a3b09ef954328fe44d8fd75e5f34
daecker <50798284+daecker@users.noreply.github.com>
2019-06-09 08:15:01 -0400

    update pytest

--- game_test.py
+++ game_test.py
@@ -1,23 +1,13 @@
 # game_test.py
 
-from game import my_message, determine_winner #use this to reach into game file and pull function from it.
-#it imports all code from the global scope - indented left - function. so we have to 
-#use convent if __name__ == "__main__":
-    #pass
+from game import my_message, determine_winner
 
 def test_example():
     assert 2 == 2
 
 def test_my_message():
-    x = my_message()
-    assert x == "HELLO"
+    assert my_message() == "HELLO"
 
-#
-#function name must begin with test_ in order to be recongized as a test
-
-#Example
-#from game import determine_winner
-#
 def test_determination_of_the_winner():
     assert determine_winner("rock", "rock") == None # represents a tie
     assert determine_winner("rock", "paper") == "paper"
